30df8e8bf0088a4238de73899efba7caab5e9fc1
parkhewoo <pkhwoo91@gmail.com>
2024-08-14 16:58:50 +0900

    git

---
+++ springAttendanceManagementSystem/src/test/java/com/kh/spring/SpringAttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
