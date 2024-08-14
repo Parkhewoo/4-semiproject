68526f15be35548e8e8bb31e9d945ac55288b1e9
parkhewoo <pkhwoo91@gmail.com>
2024-08-14 17:12:46 +0900

    git

---
+++ AttendPro/src/main/java/com/kh/AttendPro/AttendProApplication.java
@@ -0,0 +1,13 @@
+package com.kh.AttendPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendProApplication.class, args);
+	}
+
+}
---
+++ AttendPro/src/test/java/com/kh/AttendPro/AttendProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.AttendPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
