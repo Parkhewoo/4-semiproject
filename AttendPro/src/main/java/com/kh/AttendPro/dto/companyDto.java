0688659ec2587cec90c496f2b2c8fdced89a4731
poiu1061 <ryoupr1061@gmail.com>
2024-08-14 18:06:59 +0900

    dto추가

---
+++ AttendPro/src/main/java/com/kh/AttendPro/dto/companyDto.java
@@ -0,0 +1,5 @@
+package com.kh.AttendPro.dto;
+
+public class companyDto {
+
+}
---
+++ AttendPro/src/main/java/com/kh/AttendPro/dto/workerDto.java
@@ -0,0 +1,8 @@
+package com.kh.AttendPro.dto;
+
+import lombok.Data;
+
+@Data
+public class workerDto {
+	private String workerNumber;
+}
