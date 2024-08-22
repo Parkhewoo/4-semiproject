556562bdedb8c4f93b29ed93a9bdaf3f58c9370c
aldskaldsk <aldskaldsk@gmail.com>
2024-08-22 17:42:15 +0900

    page VO, Qna dto, Qna dao, Qna mapper, Qna controller

---
+++ AttendPro/src/main/java/com/kh/AttendPro/dto/QnaDto.java
@@ -0,0 +1,42 @@
+package com.kh.AttendPro.dto;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import lombok.Data;
+
+@Data
+public class QnaDto {
+	private int qnaNo;
+	private String qnaTitle;
+	private String qnaContent;
+	private String qnaWriter;
+	private Date qnaWTime;
+	private Date qnaUTime;
+	private int qnaReplies;	
+	
+	
+	//메소드 추가
+	public String getQnAWriterString() {
+		if(qnaWriter ==null) return "탈퇴한 사용자";
+		return qnaWriter;
+	}
+	
+	//java.sql.date는 LocalDate로 쉽게 변환 가능
+	//java.sql.Timestamp는 LocalDateTime으로 쉽게 변환가능
+	public String getQnAWTimeString() {
+		Timestamp stamp = new Timestamp(qnaWTime.getTime());
+		LocalDateTime time = stamp.toLocalDateTime();
+		LocalDate today = LocalDate.now();
+		if(time.toLocalDate().equals(today)) {
+			return time.format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+		else
+		return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	}
+	
+	
+}
