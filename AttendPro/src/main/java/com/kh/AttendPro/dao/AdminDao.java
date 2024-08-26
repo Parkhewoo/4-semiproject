5e7bc8c49e9dd39888111854ce38d4cc5ec1c37c
ek9290 <ke9290@naver.com>
2024-08-26 17:00:59 +0900

    시스템관리자 기능(일반관리자 수정,삭제) 기능 추가 및 데이터베이스 현황(Status) 페이지 구현

--- AttendPro/src/main/java/com/kh/AttendPro/dao/AdminDao.java
+++ AttendPro/src/main/java/com/kh/AttendPro/dao/AdminDao.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Repository;
 import com.kh.AttendPro.dto.AdminDto;
 import com.kh.AttendPro.dto.WorkerDto;
 import com.kh.AttendPro.mapper.AdminMapper;
+import com.kh.AttendPro.mapper.StatusMapper;
 import com.kh.AttendPro.vo.PageVO;
+import com.kh.AttendPro.vo.StatusVO;
 
 
 @Repository
@@ -113,6 +115,45 @@ public class AdminDao {
 	        return jdbcTemplate.queryForObject(sql, Integer.class);
 	    }
 	}
+	
+
+	public boolean updateAdminBySysadmin(AdminDto adminDto) {
+		String sql = "update admin set"
+				+ "admin_no=?"
+				+ "admin_rank=?"
+				+ "admin_email=?"
+				+ "admin_login=?";
+		Object[] data = {
+				adminDto.getAdminNo(),
+				adminDto.getAdminRank(),
+				adminDto.getAdminEmail(),
+				adminDto.getAdminLogin()
+		};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+
+
+
+	public boolean delete(String adminId) {
+		String sql = "delete admin where admin_id = ?";
+		Object[] data = {adminId};
+		return jdbcTemplate.update(sql, data)>0;
+	}
 
 	
+	
+	@Autowired
+	private StatusMapper statusMapper;
+	
+	//시스템관리자 데이터베이스현황 조회 status
+	public List<StatusVO> statusByAdminRank() {
+	    // 쿼리 문자열에 각 절 사이에 공백 추가
+	    String sql = "SELECT admin_rank title, COUNT(*) cnt "
+	               + "FROM admin "
+	               + "GROUP BY admin_rank "
+	               + "ORDER BY cnt DESC, title ASC";
+
+	    // 쿼리 실행 및 결과 반환
+	    return jdbcTemplate.query(sql, statusMapper);
+	}
 }
