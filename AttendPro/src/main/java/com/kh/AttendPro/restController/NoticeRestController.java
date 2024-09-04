0f3df069b9d7a73a2051b48b52a2b189acc25931
aldskaldsk <aldskaldsk@gmail.com>
2024-09-04 12:54:29 +0900

    notice 사진 들어가게 수정

--- AttendPro/src/main/java/com/kh/AttendPro/restController/NoticeRestController.java
+++ AttendPro/src/main/java/com/kh/AttendPro/restController/NoticeRestController.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.AttendPro.dao.NoticeDao;
 import com.kh.AttendPro.error.TargetNotFoundException;
 import com.kh.AttendPro.service.AttachmentService;
 
-@CrossOrigin(origins = {"http://127.0.0.1:5500"})
+
 @RestController
 @RequestMapping("/rest/notice")
 public class NoticeRestController {
