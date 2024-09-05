b2d3432af887ad948bf7c6a9f087b2608b3eedf4
Parkhewoo <pkhwoo91@gmail.com>
2024-09-06 07:56:54 +0900

    인터셉터 수정중

--- AttendPro/src/main/java/com/kh/AttendPro/configuration/InterceptorConfiguration.java
+++ AttendPro/src/main/java/com/kh/AttendPro/configuration/InterceptorConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.kh.AttendPro.interceptor.AdminInterceptor;
 import com.kh.AttendPro.interceptor.MemberInterceptor;
+import com.kh.AttendPro.interceptor.QnaOwnerInterceptor;
 import com.kh.AttendPro.interceptor.SysAdminInterceptor;
 import com.kh.AttendPro.interceptor.WorkerInterceptor;
 
@@ -19,29 +21,39 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
     private SysAdminInterceptor sysAdminInterceptor;
     
     @Autowired
+    private AdminInterceptor adminInterceptor;
+    
+    @Autowired
     private WorkerInterceptor workerInterceptor;
+    
+    @Autowired
+    private QnaOwnerInterceptor qnaOwnerInterceptor;
     
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         
-//        registry.addInterceptor(sysAdminInterceptor)
-//                .addPathPatterns("/admin/company/set",
-//                						"/admin/company/insert",
-//                						"/admin/worker/add",
-//                						"/admin/worker/list",
-//                						"/admin/worker/edit",
-//                						"/admin/worker/delete",
-//                						"/admin/worker/addfinish",
-//                						"/admin/password",
-//                						"/admin/mypage",
-//                						"/admin/change"
-//                						);
-//          
-               
-//        
-//
-//        registry.addInterceptor(workerInterceptor)
-//                .addPathPatterns("/admin/**", "/sysadmin/**");
+        registry.addInterceptor(sysAdminInterceptor)
+                .addPathPatterns("/sysadmin/*");
+          
+        registry.addInterceptor(adminInterceptor)
+        		.addPathPatterns("/admin/**")
+        		.excludePathPatterns("/admin/login", "/admin/join"
+        				,"/admin/findPw","/admin/findPwFinish", 
+        				"/admin/resetPw", "/admin/resetPwComplete","/admin/logout");
+    
+        
+        registry.addInterceptor(qnaOwnerInterceptor)
+							.addPathPatterns( "/qna/**");
+        
+        registry.addInterceptor(workerInterceptor)
+        .addPathPatterns( "/worker/**")
+        .excludePathPatterns("/worker/login"
+        		,"/worker/findPw","/worker/findPwFinish", 
+        		"/worker/resetPw", "/worker/resetPwComplete");
+   
+        registry.addInterceptor(memberInterceptor)
+        .addPathPatterns("/admin/login", "/worker/login");
+        
     }
 }
